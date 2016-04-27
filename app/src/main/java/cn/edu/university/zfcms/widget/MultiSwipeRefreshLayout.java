f603584b1c4679fb0046036667c335812d47f69b
hjw <huangjiawen@innlab.net>
2016-04-27 18:35:54 +0800

    修改抽屉 UI

--- app/src/main/java/cn/edu/university/zfcms/widget/MultiSwipeRefreshLayout.java
+++ app/src/main/java/cn/edu/university/zfcms/widget/MultiSwipeRefreshLayout.java
@@ -1,68 +1,109 @@
+/*
+ * Copyright 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cn.edu.university.zfcms.widget;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AbsListView;
 
-import cn.edu.university.zfcms.R;
-
+/**
+ * A descendant of {@link SwipeRefreshLayout} which supports multiple
+ * child views triggering a refresh gesture. You set the views which can trigger the gesture via
+ * {@link #setSwipeableChildren(int...)}, providing it the child ids.
+ */
 public class MultiSwipeRefreshLayout extends SwipeRefreshLayout {
-    private CanChildScrollUpCallback mCanChildScrollUpCallback;
 
-    private Drawable mForegroundDrawable;
+    private View[] mSwipeableChildren;
 
     public MultiSwipeRefreshLayout(Context context) {
-        this(context, null);
+        super(context);
     }
 
     public MultiSwipeRefreshLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        final TypedArray a = context.obtainStyledAttributes(attrs,
-                R.styleable.MultiSwipeRefreshLayout, 0, 0);
-
-        this.mForegroundDrawable = a.getDrawable(R.styleable.MultiSwipeRefreshLayout_foreground);
-        if (this.mForegroundDrawable != null) {
-            this.mForegroundDrawable.setCallback(this);
-            setWillNotDraw(false);
-        }
-
-        a.recycle();
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        if (this.mForegroundDrawable != null) {
-            this.mForegroundDrawable.setBounds(0, 0, w, h);
+    /**
+     * Set the children which can trigger a refresh by swiping down when they are visible. These
+     * views need to be a descendant of this view.
+     */
+    public void setSwipeableChildren(final int... ids) {
+        assert ids != null;
+
+        // Iterate through the ids and find the Views
+        mSwipeableChildren = new View[ids.length];
+        for (int i = 0; i < ids.length; i++) {
+            mSwipeableChildren[i] = findViewById(ids[i]);
         }
     }
 
-    @Override
-    public void draw(@NonNull Canvas canvas) {
-        super.draw(canvas);
-        if (this.mForegroundDrawable != null) {
-            this.mForegroundDrawable.draw(canvas);
-        }
-    }
+    // BEGIN_INCLUDE(can_child_scroll_up)
 
-    public void setCanChildScrollUpCallback(CanChildScrollUpCallback canChildScrollUpCallback) {
-        this.mCanChildScrollUpCallback = canChildScrollUpCallback;
-    }
-
-    public static interface CanChildScrollUpCallback {
-        public boolean canSwipeRefreshChildScrollUp();
-    }
-
+    /**
+     * This method controls when the swipe-to-refresh gesture is triggered. By returning false here
+     * we are signifying that the view is in a state where a refresh gesture can start.
+     * <p>
+     * <p>As {@link SwipeRefreshLayout} only supports one direct child by
+     * default, we need to manually iterate through our swipeable children to see if any are in a
+     * state to trigger the gesture. If so we return false to start the gesture.
+     */
     @Override
     public boolean canChildScrollUp() {
-        if (mCanChildScrollUpCallback != null) {
-            return mCanChildScrollUpCallback.canSwipeRefreshChildScrollUp();
+        if (mSwipeableChildren != null && mSwipeableChildren.length > 0) {
+            // Iterate through the scrollable children and check if any of them can not scroll up
+            for (View view : mSwipeableChildren) {
+                if (view != null && view.isShown() && !canViewScrollUp(view)) {
+                    // If the view is shown, and can not scroll upwards, return false and start the
+                    // gesture.
+                    return false;
+                }
+            }
         }
-        return super.canChildScrollUp();
+        return true;
     }
+    // END_INCLUDE(can_child_scroll_up)
 
+    // BEGIN_INCLUDE(can_view_scroll_up)
+
+    /**
+     * Utility method to check whether a {@link View} can scroll up from it's current position.
+     * Handles platform version differences, providing backwards compatible functionality where
+     * needed.
+     */
+    private static boolean canViewScrollUp(View view) {
+        if (android.os.Build.VERSION.SDK_INT >= 14) {
+            // For ICS and above we can call canScrollVertically() to determine this
+            return ViewCompat.canScrollVertically(view, -1);
+        } else {
+            if (view instanceof AbsListView) {
+                // Pre-ICS we need to manually check the first visible item and the child view's top
+                // value
+                final AbsListView listView = (AbsListView) view;
+                return listView.getChildCount() > 0 &&
+                        (listView.getFirstVisiblePosition() > 0
+                                || listView.getChildAt(0).getTop() < listView.getPaddingTop());
+            } else {
+                // For all other view types we just check the getScrollY() value
+                return view.getScrollY() > 0;
+            }
+        }
+    }
+    // END_INCLUDE(can_view_scroll_up)
 }
