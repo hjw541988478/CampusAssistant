3be64dfdae7dd3f1b5c98472b2bdac5f47c002bb
garvin_yellow <huangjiawendanny@gmail.com>
2017-04-28 15:49:01 +0800

    整理 gradle 配置

--- app/src/main/java/cn/edu/university/zfcms/app/ZfsoftCampusAsstApp.java
+++ app/src/main/java/cn/edu/university/zfcms/app/ZfsoftCampusAsstApp.java
@@ -1,9 +1,12 @@
 package cn.edu.university.zfcms.app;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 import android.util.Log;
 
 import com.facebook.stetho.Stetho;
+import com.github.moduth.blockcanary.BlockCanary;
 import com.squareup.leakcanary.LeakCanary;
 
 import cn.edu.university.zfcms.util.PreferenceUtil;
@@ -13,18 +16,20 @@ import cn.edu.university.zfcms.util.PreferenceUtil;
  */
 public class ZfsoftCampusAsstApp extends Application{
 
-    private static final String TAG = ZfsoftCampusAsstApp.class.getSimpleName();
-
     private static ZfsoftCampusAsstApp appContext;
 
     public static ZfsoftCampusAsstApp getInstance() {
         return appContext;
     }
 
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(TAG, "onCreate() called");
         appContext = this;
         PreferenceUtil.init(getApplicationContext());
         Stetho.initializeWithDefaults(this);
